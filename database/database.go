53be52485a8fbdacc67e205983d757e6ef8c9390
Prakarshsingh5 <prakarshsinghlmp@gmail.com>
2025-11-01 13:05:39 +0530

    Added the function for getting leads deleting leads

--- database/database.go
+++ database/database.go
@@ -1,8 +1,10 @@
 package database
 
 import (
+	
 		"github.com/jinzhu/gorm"
 		"github.com/jinzhu/gorm/dialects/sqlite"
+		"github.com/gofiber/fiber"
 )
 
 var (
--- main.go
+++ main.go
@@ -1,21 +1,30 @@
 package main
 
 import (
-		"github/PrakarshSingh5/go-fiber-crm-basic/database"
-		"github.com/gofiber/fiber"
+	"fmt"
+	"github/PrakarshSingh5/go-fiber-crm-basic/lead"
+	"github/PrakarshSingh5/go-fiber-crm-basic/database"
+	"github.com/gofiber/fiber"
+	"github.com/jinzhu/gorm"
 )
 
-func setupRoutes(app *fiber.App){
-	app.Get(GetLeads)
-	app.Get(GetLead)
-	app.Post(NewLead)
-	app.Delete(DeleteLead)
+func setupRoutes(app *fiber.App) {
+	app.Get("/api/v1/lead",lead.GetLeads)
+	app.Get("/api/v1/lead/:id",lead.GetLead)
+	app.Post("/api/v1/lead",lead.NewLead)
+	app.Delete("/api/v1/lead/:id",lead.DeleteLead)
 }
-func initDatabase(){
+func initDatabase() {
 	var err error
-	database.DBConn
+	database.DBConn, err = gorm.Open("sqlite3", "leads.db")
+	if err != nil {
+		panic("failed to connect database")
+	}
+	fmt.Println("Connection opened to database")
+	database.DBConn.AutoMigrate(&lead.Lead{})
+	fmt.Println("Database Migrated")
 }
-func main(){
+func main() {
 	app := fiber.New()
 	initDatabase()
 	setupRoutes(app)
